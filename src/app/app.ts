2fa3e8de05f60e3b4acb6a6afca06d18dec1536e
areyesc30-pixel <areyesc30@miumg.edu.gt>
2026-09-20 19:16:37 -0600

    Contador - Práctica

---
+++ src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App  {
+  contador: number = 0;
+
+  aumentar(): void {
+    this.contador++;
+  }
+  disminuir ():void{
+    this.contador--;
+    
+  }
+}
+
